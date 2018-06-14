ec0590dcfcf9d8ab3fd1cb6929f1b9e15ad541d7
Bin Lin <binlin2000@gmail.com>
2018-06-14 14:03:15 -0400

    play some more

--- app/src/main/java/com/example/binlin/uielementsexamples/MainActivity.java
+++ app/src/main/java/com/example/binlin/uielementsexamples/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.binlin.uielementsexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
     private TextView textView;
     private Button button;
+    private Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView2);
         editText = findViewById(R.id.editText);
         button = findViewById(R.id.button);
+        aSwitch = findViewById(R.id.switch2);
 
         textView.setText("This Is Just A Test");
         setListeners();
@@ -35,5 +40,17 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText(editText.getText());
             }
         });
-    }
+        aSwitch.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if(aSwitch.isChecked()){
+                    Toast.makeText(MainActivity.this, "IsOn", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "IsOff", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+}
+
 }
