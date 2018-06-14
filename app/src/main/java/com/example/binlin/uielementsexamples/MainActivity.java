7bf6f1fc34626e62a74a39ff5c74c5ba6c3b4c01
Bin Lin <binlin2000@gmail.com>
2018-06-14 13:37:46 -0400

    just play with it

--- app/src/main/java/com/example/binlin/uielementsexamples/MainActivity.java
+++ app/src/main/java/com/example/binlin/uielementsexamples/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.binlin.uielementsexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView2);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+
+        textView.setText("This Is Just A Test");
+        setListeners();
+
+
+    }
+
+    private void setListeners() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // take input from editText and set as value for textView
+                textView.setText(editText.getText());
+            }
+        });
     }
 }
